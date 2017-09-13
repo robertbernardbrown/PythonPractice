1b9673dac7462ce3b300f9550572c58081c1e542
Bob Brown <robertbernardbrown@gmail.com>
2017-09-12 22:15:40 -0400

    practice making login page in Discover Flask repo

--- DiscoverFlask/app.py
+++ DiscoverFlask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -7,10 +7,19 @@ app = Flask(__name__)
 def home():
 		return "Hello, world"
 
-
 @app.route('/welcome')
 def welcome():
 		return render_template("welcome.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'invalid credentials. Please try again.'
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
