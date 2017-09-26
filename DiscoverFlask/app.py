d29c4283ab7b4ef0bf5fd4cb2b766d0567a86cc8
Bob Brown <robertbernardbrown@gmail.com>
2017-09-25 22:44:27 -0400

    In progress. Create a database where information and posts are stored to be accessed once logged into web app

--- DiscoverFlask/app.py
+++ DiscoverFlask/app.py
@@ -29,16 +29,7 @@ def home():
 def welcome():
 		return render_template("welcome.html")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'invalid credentials. Please try again.'
-		else:
-			session['logged_in'] = True
-			flash('you were just logged in')
-			return redirect(url_for('home'))
+@app.route('/login', methods=['GET', 'POST'])			return redirect(url_for('home'))
 	return render_template('login.html', error=error)
 
 @app.route('/logout')
