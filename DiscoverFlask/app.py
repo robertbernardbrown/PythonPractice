6cb2e2b83183327435a0e1ca1f49f1bc8a64d96b
Bob Brown <robertbernardbrown@gmail.com>
2017-09-26 21:34:20 -0400

    Add a database to the python app for users to log in and view. Use Sqlite3 to create database of information.

--- DiscoverFlask/app.py
+++ DiscoverFlask/app.py
@@ -1,12 +1,14 @@
 #import flask class from Flask module
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+import sqlite3
 
 #create the application object
 app = Flask(__name__)
 
 #would ideally be created by a random number generator
 app.secret_key = "my precious" 
+app.database = "sample.db"
 
 #login recquired decorator
 def login_required(f):
@@ -23,14 +25,29 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-		return render_template('index.html')
+		g.db = connect_db()
+		cur = g.db.execute('select * from posts')
+		posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+		g.db.close()
+		return render_template('index.html', posts=posts)
 
 @app.route('/welcome')
 def welcome():
 		return render_template("welcome.html")
 
-@app.route('/login', methods=['GET', 'POST'])			return redirect(url_for('home'))
-	return render_template('login.html', error=error)
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if (request.form['username'] != 'admin') \
+                or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route('/logout')
 @login_required
@@ -38,6 +55,10 @@ def logout():
 	session.pop('logged_in', None)
 	flash('You were just logged out')
 	return redirect(url_for('welcome'))
+
+def connect_db():
+	return sqlite3.connect(app.database)
+
 
 #start the server with a run method
 if __name__ == '__main__':
